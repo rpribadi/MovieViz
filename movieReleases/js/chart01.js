dc7369782ee53efe990e106150c28e4f6f7f1007
Riki Pribadi <pribadi.riki@gmail.com>
2014-04-13 20:26:33 +0800

    Fix movie release chart query.

--- movieReleases/js/chart01.js
+++ movieReleases/js/chart01.js
@@ -14,19 +14,36 @@
 		var height = $( element ).height() - padding.top - padding.bottom;
 
 		var sizeScale = d3.scale.linear()
-			.range([2, 15]);
+			.range([2, 10]);
 
 		var colorScale =   d3.scale.log()
 	        .interpolate(d3.interpolateHsl)
 	        .range(["hsl(176, 100%, 50%)","hsl(340, 100%, 50%)"]);
 		
-		var x = d3.scale.linear()
+		var x = d3.time.scale()
 			.range( [ 0, width ] );
-
+		
 		var y = d3.scale.linear()
 		    .range( [ height, 0 ] );
 	
 		var xAxis = d3.svg.axis()
+//			.ticks( d3.time.months, 2)
+			.tickFormat(d3.time.format('%b %d'))
+			.ticks(function() { return [
+			     new Date( Date.parse("2013-01-31") ),
+			     new Date( Date.parse("2013-02-28") ),
+			     new Date( Date.parse("2013-03-31") ),
+			     new Date( Date.parse("2013-04-30") ),
+			     new Date( Date.parse("2013-05-31") ),
+			     new Date( Date.parse("2013-06-30") ),
+			     new Date( Date.parse("2013-07-31") ),
+			     new Date( Date.parse("2013-08-31") ),
+			     new Date( Date.parse("2013-09-30") ),
+			     new Date( Date.parse("2013-10-31") ),
+			     new Date( Date.parse("2013-11-30") ),
+			     new Date( Date.parse("2013-12-31") )
+			     ]
+			})
 		    .scale(x)
 		    .orient("bottom");
 	
@@ -36,7 +53,7 @@
 	
 		var line = d3.svg.line()
 			.interpolate("monotone")
-		    .x(function(d) { return x( d.date ); })
+		    .x(function(d) { return x( new Date(Date.parse(d.date)) ); })
 		    .y(function(d) { return y( d.total ); });
 
 		var chart = svg.append("g")
@@ -48,9 +65,10 @@
 			});
 			
 			var maxTotal = d3.max(data, function(d) { return d.total; });
-			x.domain( [ 0, data.length ] );
+			x.domain( [ new Date(Date.parse("2013-01-01")), new Date(Date.parse("2013-12-31")) ] );
 			y.domain( [ 0, maxTotal + 5 ] );
 			sizeScale.domain([ 0, maxTotal ]);
+
 			
 //			console.log(x.domain(), x.rangeRoundBands())
 
@@ -62,18 +80,11 @@
 				.style("text-anchor", "end")
 				.attr("dx", "-.2em")
 				.attr("dy", ".3em")
-				.text("")
+
 				.attr("transform", function(d) {
 				    return "rotate(-35)" 
 			    });
 
-			chart.select("g.x.axis")
-				.append("text")
-					.attr("y", -20)
-					.attr("x", width + 5)
-					.attr("dy", "2.71em")
-					.style("text-anchor", "end")
-					.text("Date");
 
 			chart.append("g")
 			  .attr("class", "y axis")
@@ -98,7 +109,7 @@
 				   		return element.replace("#", "") + "-" + d.date;
 				   })
 				   .attr("cx", function(d, i) {
-				   		return x(i);
+				   		return x( new Date( Date.parse(d.date) ) );
 				   })
 				   .attr("cy", function(d) {
 				   		return y(d.total);
@@ -110,7 +121,7 @@
 					   $("#chart-01-infobox").css({
 							   	"display": "auto",
 							   	"visibility": "visible",
-								"left": x(i) + 60,
+								"left": x( new Date( Date.parse(item.date) ) ) + 60,
 								"top": y(item.total)
 					   		})
 							.html("<strong>" + item.date + "</strong><br />Total: " + item.total);

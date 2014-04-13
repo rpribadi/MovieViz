30694601fc26d882536448180b02cc9c7a3c7c2e
Riki Pribadi <pribadi.riki@gmail.com>
2014-04-13 13:09:21 +0800

    Add number of movie releases by date viz.

---
+++ movieReleases/js/chart01.js
@@ -0,0 +1,131 @@
+(function(window, $, d3) {
+
+	"use strict";
+
+	var Chart01 = function(element, filename) {
+		var svg = d3.select(element);
+		var padding = {
+			top: 50,
+			bottom: 50,
+			left: 50,
+			right: 50,
+		};
+		var width = $( element ).width() - padding.left - padding.right;
+		var height = $( element ).height() - padding.top - padding.bottom;
+
+		var sizeScale = d3.scale.linear()
+			.range([2, 15]);
+
+		var colorScale =   d3.scale.log()
+	        .interpolate(d3.interpolateHsl)
+	        .range(["hsl(176, 100%, 50%)","hsl(340, 100%, 50%)"]);
+		
+		var x = d3.scale.linear()
+			.range( [ 0, width ] );
+
+		var y = d3.scale.linear()
+		    .range( [ height, 0 ] );
+	
+		var xAxis = d3.svg.axis()
+		    .scale(x)
+		    .orient("bottom");
+	
+		var yAxis = d3.svg.axis()
+		    .scale(y)
+		    .orient("left");
+	
+		var line = d3.svg.line()
+			.interpolate("monotone")
+		    .x(function(d) { return x( d.date ); })
+		    .y(function(d) { return y( d.total ); });
+
+		var chart = svg.append("g")
+	    	.attr("transform", "translate(" + padding.left + "," + padding.top + ")");
+		
+		d3.tsv(filename, function( error, data ) {
+			data.forEach( function( item, i ) {
+				item.total = parseInt(item.total);
+			});
+			
+			var maxTotal = d3.max(data, function(d) { return d.total; });
+			x.domain( [ 0, data.length ] );
+			y.domain( [ 0, maxTotal + 5 ] );
+			sizeScale.domain([ 0, maxTotal ]);
+			
+//			console.log(x.domain(), x.rangeRoundBands())
+
+			chart.append("g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis)
+			.selectAll("text")  
+				.style("text-anchor", "end")
+				.attr("dx", "-.2em")
+				.attr("dy", ".3em")
+				.text("")
+				.attr("transform", function(d) {
+				    return "rotate(-35)" 
+			    });
+
+			chart.select("g.x.axis")
+				.append("text")
+					.attr("y", -20)
+					.attr("x", width + 5)
+					.attr("dy", "2.71em")
+					.style("text-anchor", "end")
+					.text("Date");
+
+			chart.append("g")
+			  .attr("class", "y axis")
+			  .call(yAxis)
+			.append("text")
+			  .attr("transform", "rotate(-90)")
+			  .attr("y", 6)
+			  .attr("dy", ".71em")
+			  .style("text-anchor", "end")
+			  .text("Total");
+
+			var barWidth = width/data.length;
+		    //Create circles
+			chart.append("g")
+				.attr("transform", "translate(" + barWidth + ",0)")
+			.selectAll("circle")
+				   .data(data)
+				   .enter()
+				   .append("circle")
+				   .attr("id", function(d, i) {
+					   	
+				   		return element.replace("#", "") + "-" + d.date;
+				   })
+				   .attr("cx", function(d, i) {
+				   		return x(i);
+				   })
+				   .attr("cy", function(d) {
+				   		return y(d.total);
+				   })
+				   .attr("r", function(d) { return sizeScale(d.total)})
+				   .attr("fill", function(d,i) { return colorScale(d.total); })
+				   .on("mouseover", function( item, i ) {
+					   console.log( item );
+					   $("#chart-01-infobox").css({
+							   	"display": "auto",
+							   	"visibility": "visible",
+								"left": x(i) + 60,
+								"top": y(item.total)
+					   		})
+							.html("<strong>" + item.date + "</strong><br />Total: " + item.total);
+					   
+					   $(element + "-" + item.date).css("stroke", "#666").css("stroke-width", 2)
+				   })
+				   .on("mouseout", function( item ) {
+					   $("#chart-01-infobox").css("display", "none");
+					   $(element + "-" + item.date).css("stroke", "none")
+				   });
+
+		});
+
+	};
+
+	window.Chart01 = Chart01;
+
+}(window, $, d3));
